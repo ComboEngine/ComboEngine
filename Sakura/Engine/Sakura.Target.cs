380a5dd20203a574f992524b5d5ffe06b6431ab4
Piotr Chudziński <73133130+tecnessino@users.noreply.github.com>
2023-05-28 16:17:02 +0200

    Added DirectX to libraries

--- Sakura/Engine/Sakura.Target.cs
+++ Sakura/Engine/Sakura.Target.cs
@@ -1,16 +1,20 @@
 using System;
+using System.Collections.Generic;
 using Sakura.BuildTools;
 
 public class SakuraEngine : BuildTarget {
     public void Build() {
         this.SetName("Sakura.Engine");
         this.AddSourceFolder("Source");
-        this.AddIncludeDirectory("Include");
         this.AddIncludeDirectory("Source");
-        this.AddLibraryDirectory("Library");
         this.SetBuildType(BuildType.Executable);
 
+        List<string> libraries = new List<string>();
+        libraries.Add("DirectX");
 
-        this.LinkLibrary("OpenGL32.lib");
+        foreach(string library in libraries) {
+            this.AddIncludeDirectory("Include\\" + library);
+            this.AddLibraryDirectory("Library\\" + library);
+        }
     }
 }
